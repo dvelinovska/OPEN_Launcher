51e6dba00508acaeb9912dec0a2f9191f2245136
Dragica Nikolova <dragica.nikolova@endava.com>
2016-04-05 09:14:44 +0200

    start screen 3

--- src/app/components/home/HomeComponent.ts
+++ src/app/components/home/HomeComponent.ts
@@ -1,19 +1,10 @@
 import {Component} from 'angular2/core';
 
-import {UserService} from '../../shared/services/UserService';
-import {AuthService} from '../../shared/services/AuthService';
-import {AlertingService} from '../alerting/AlertingService';
-import {User} from '../../shared/models/User';
-
 @Component({
-    selector: 'home',
-    templateUrl: `./app/components/home/home.html`
+  selector: 'home',
+  templateUrl: `./app/components/home/home.html`
 })
 export class HomeComponent {
-    public allUsers: User[] = new Array<User>();
-    public newUser: User = new User();
-    public zapoznajSeSoKomp = "Cause and Effect";
-    public ucimeSoKomp = ['Sets', 'Who is hiding', 'Puzzle/Halves', 'Me and my home', 'Story'];
-
-    constructor(private authService: AuthService) { }
+  public zapoznajSeSoKomp = "Cause and Effect";
+  public ucimeSoKomp = ['Sets', 'Who is hiding', 'Puzzle/Halves', 'Me and my home', 'Story'];
 }
