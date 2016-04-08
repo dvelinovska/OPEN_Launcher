b6f8582885081727d87bf8d2315fa891292925bb
despotovska <alex_despotovska@yahoo.com>
2016-04-08 17:38:00 +0200

    Pipe's selector renamed

--- src/app/components/login/LoginComponent.ts
+++ src/app/components/login/LoginComponent.ts
@@ -8,59 +8,59 @@ import {User} from '../../shared/models/User';
 import {UsersPipe} from '../../shared/pipes/UsersPipe';
 
 @Component({
-  selector: 'login',
-  directives: [RouterLink],
-  pipes: [UsersPipe],
-  templateUrl: `./app/components/login/login.html`
+    selector: 'login',
+    directives: [RouterLink],
+    pipes: [UsersPipe],
+    templateUrl: `./app/components/login/login.html`
 })
 export class LoginComponent {
-  public allUsers: User[] = new Array<User>();
-  public selectedUser: User = new User();
-  public usernamePipe = '';
+    public allUsers: User[] = new Array<User>();
+    public selectedUser: User = new User();
+    public query = '';
 
-  constructor(
-    private alertingService: AlertingService,
-    private userService: UserService,
-    private authService: AuthService,
-    private router: Router) {
+    constructor(
+        private alertingService: AlertingService,
+        private userService: UserService,
+        private authService: AuthService,
+        private router: Router) {
 
-    this.setAllUsers();
-  }
-
-  setAllUsers(): void {
-    this.userService.getAllUsers()
-      .subscribe(data => this.allUsers = data,
-      err => this.alertingService.addDanger('Грешка при вчитување на корисниците.'));
-  }
-
-  deleteUser(): void {
-    this.userService.deleteUser(this.selectedUser.name)
-      .subscribe(data => {
-        this.allUsers = data;
-        this.selectedUser = new User();
-        this.alertingService.addSuccess('Профилот е успешно избришан.');
-      }, err => {
-        this.alertingService.addDanger('Грешка при бришење на профилот.');
-      });
-  }
-
-  deleteCancelled(): void {
-    this.alertingService.addInfo('Бришењето е откажано.');
-  }
-
-  login(): void {
-    if (!this.authService.login(this.selectedUser.name)) {
-      this.alertingService.addDanger('Корисникот не е валиден.');
-    } else {
-      this.router.navigate(['/Home']);
+        this.setAllUsers();
     }
-  }
 
-  selectUser(user: User): void {
-    this.selectedUser = user;
-  }
+    setAllUsers(): void {
+        this.userService.getAllUsers()
+            .subscribe(data => this.allUsers = data,
+            err => this.alertingService.addDanger('Грешка при вчитување на корисниците.'));
+    }
 
-  shouldApplySelectedUserLayout(user: User): boolean {
-    return this.selectedUser === user;
-  }
+    deleteUser(): void {
+        this.userService.deleteUser(this.selectedUser.name)
+            .subscribe(data => {
+                this.allUsers = data;
+                this.selectedUser = new User();
+                this.alertingService.addSuccess('Профилот е успешно избришан.');
+            }, err => {
+                this.alertingService.addDanger('Грешка при бришење на профилот.');
+            });
+    }
+
+    deleteCancelled(): void {
+        this.alertingService.addInfo('Бришењето е откажано.');
+    }
+
+    login(): void {
+        if (!this.authService.login(this.selectedUser.name)) {
+            this.alertingService.addDanger('Корисникот не е валиден.');
+        } else {
+            this.router.navigate(['/Home']);
+        }
+    }
+
+    selectUser(user: User): void {
+        this.selectedUser = user;
+    }
+
+    shouldApplySelectedUserLayout(user: User): boolean {
+        return this.selectedUser === user;
+    }
 }
--- src/app/shared/pipes/UsersPipe.ts
+++ src/app/shared/pipes/UsersPipe.ts
@@ -3,10 +3,10 @@ import {Pipe, PipeTransform} from 'angular2/core';
 import {User} from '../models/User';
 
 @Pipe({
-  name: 'usersPipe'
+    name: 'usernameContains'
 })
 export class UsersPipe implements PipeTransform {
-  transform(value: User[], args?: any[]): any {
-    return value.filter(item => item.name.toLowerCase().indexOf(args[0].toLowerCase()) !== -1);
-  }
+    transform(value: User[], args?: any[]): any {
+        return value.filter(item => item.name.toLowerCase().indexOf(args[0].toLowerCase()) !== -1);
+    }
 }
